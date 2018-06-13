58e0cc197e759d122e15d89ba950151bf4e29776
guppy0130 <guppy0130@yahoo.com>
2018-06-12 21:05:27 -0700

    fix if dist doesn't exist and actual chapter logging

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,11 @@ marked.setOptions({
 });
 
 gulp.task('del', () => {
+    try {
+        fs.statSync('./dist');
+    } catch {
+        fs.mkdirSync('./dist');
+    }
     return del(['./dist/*', '!./dist']);
 });
 
@@ -27,50 +32,54 @@ gulp.task('handlebars', (done) => {
     done();
 });
 
-gulp.task('pages', () => {
+gulp.task('pages', (done) => {
     let template;
     fs.readFile('./src/views/templates/page.hbs', 'utf8', (err, templateString) => {
         template = handlebars.compile(templateString);
+        return gulp.src('./src/content/**/*.md')
+            .pipe(markdown(marked))
+            .pipe(tap((file, t) => {
+                file.contents = Buffer.from(template(JSON.parse(file.contents.toString())));
+            })).pipe(rename({
+                extname: '.html'
+            })).pipe(gulp.dest('./dist'))
+            .on('finish', () => {
+                done();
+            });
     });
-    return gulp.src('./src/content/**/*.md')
-        .pipe(markdown(marked))
-        .pipe(tap((file, t) => {
-            file.contents = Buffer.from(template(JSON.parse(file.contents.toString())));
-        })).pipe(rename({
-            extname: '.html'
-        })).pipe(gulp.dest('./dist'));
 });
 
 gulp.task('chapters', (done) => {
     let directory = {},
         template;
     directory['title'] = 'Super duper testing';
+    directory['chapters'] = {};
     fs.readFile('./src/views/templates/chapter.hbs', 'utf8', (err, templateString) => {
         template = handlebars.compile(templateString);
-    });
-    return gulp.src('./src/content/**/*.md')
-        .pipe(tap((file, t) => {
-            let relativePath = path.relative(__dirname + '/src/content', file.path);
-            let relativeArray = relativePath.split(path.sep);
+        return gulp.src('./src/content/**/*.md')
+            .pipe(tap((file, t) => {
+                let relativePath = path.relative(__dirname + '/src/content', file.path);
+                let relativeArray = relativePath.split(path.sep);
 
-            if (relativeArray.length >= 3) {
-                throw new Error('only files one folder deep are allowed.');
-            }
-            if (!(relativeArray[0] in directory)) {
-                directory[relativeArray[0]] = [];
-            }
-            directory[relativeArray[0]].push(relativeArray[1]);
-        })).on('finish', () => {
-            console.log(directory);
-            let html = template(directory);
-            fs.writeFile('./dist/index.html', html, (err) => {
-                if (err) {
-                    throw err;
-                } else {
-                    return done();
+                if (relativeArray.length >= 3) {
+                    throw new Error('only files one folder deep are allowed.');
                 }
+                if (!(relativeArray[0] in directory['chapters'])) {
+                    directory['chapters'][relativeArray[0]] = [];
+                }
+                directory['chapters'][relativeArray[0]].push(relativeArray[1]);
+            })).on('finish', () => {
+                console.log(directory);
+                let html = template(directory);
+                fs.writeFile('./dist/index.html', html, (err) => {
+                    if (err) {
+                        throw err;
+                    } else {
+                        return done();
+                    }
+                });
             });
-        });
+    });
 
 });
 
